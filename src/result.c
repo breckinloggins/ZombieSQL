7863e33a048780332236ecd5caa3373b4c12f84b
breckinloggins <breckinloggins@gmail.com>
2011-05-13 16:45:08 -0500

    Removing deleted files

--- src/result.c
+++
@@ -1,9 +0,0 @@
-//
-//  result.c
-//  ZombieSQL
-//
-//  Created by Benjamin Loggins on 5/8/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "result.h"
--- src/result.h
+++
@@ -1,7 +0,0 @@
-//
-//  result.h
-//  ZombieSQL
-//
-//  Created by Benjamin Loggins on 5/8/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
